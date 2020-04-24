60fb088a822f9079cbed06f7d352ba8b9a2858b1
Qianmian Yu <im.qianmian.yu@gmail.com>
2020-04-24 17:41:51 +0800

    can read object

--- src/main.rs
+++ src/main.rs
@@ -18,9 +18,20 @@ enum SubCommand {
         #[clap(name = "PATH", default_value = ".", parse(from_os_str))]
         path: PathBuf,
     },
+    CatFile(CatFile),
     /// Add file contents to the index
     #[clap(name = "add")]
     Add(Add),
+}
+
+#[derive(Clap)]
+struct CatFile {
+    #[clap(name = "SHA")]
+    sha: String,
+}
+
+fn cat_file(args: CatFile) {
+    GitRepository::read_object(&args.sha);
 }
 
 #[derive(Clap)]
@@ -31,6 +42,8 @@ fn main() {
 
     match opts.subcmd {
         SubCommand::Init { path } => GitRepository::init(path),
+        // SubCommand::CatFile { sha } => GitRepository::read_object(&sha),
+        SubCommand::CatFile(args) => cat_file(args),
         SubCommand::Add(_add) => println!("add!"),
     }
 }
--- src/repo.rs
+++ src/repo.rs
@@ -1,6 +1,10 @@
 use std::fs;
-use std::fs::create_dir_all;
+use std::fs::{create_dir_all, File};
+use std::io::BufReader;
+use std::io::prelude::*;
 use std::path::{Path, PathBuf};
+
+use flate2::bufread::ZlibDecoder;
 
 pub struct GitRepository {
     worktree: PathBuf,
@@ -15,6 +19,13 @@ const INIT_DIRS: &[&str] = &[
 
 /// A git repository
 impl GitRepository {
+    pub fn default() -> GitRepository {
+        GitRepository {
+            worktree: PathBuf::from("."),
+            git_dir: PathBuf::from(".git"),
+        }
+    }
+
     pub fn init(mut worktree: PathBuf) {
         worktree = worktree.canonicalize().unwrap();
         if !worktree.is_dir() {
@@ -35,16 +46,73 @@ impl GitRepository {
 
         // Do initialization.
         for dir in INIT_DIRS.iter().map(PathBuf::from) {
-            create_dir_all(repo.repo_file(dir));
+            create_dir_all(repo.repo_file(dir)).unwrap();
         }
 
-        fs::write(repo.repo_file("HEAD"), "ref: refs/heads/master\n");
-        fs::write(repo.repo_file("description"), "Unnamed repository; edit this file 'description' to name the repository.\n");
+        fs::write(repo.repo_file("HEAD"), "ref: refs/heads/master\n").unwrap();
+        fs::write(repo.repo_file("description"), "Unnamed repository; edit this file 'description' to name the repository.\n").unwrap();
         // TODO configuration file, maybe with rust-ini(https://github.com/zonyitoo/rust-ini)
+    }
+
+    pub fn read_object(sha: &str) {
+        if sha.len() != 40 || !sha.is_ascii() {
+            panic!("Invalid SHA-1 Value")
+        }
+
+        let repo = GitRepository::default();
+        let mut dirs = vec!["objects"];
+        dirs.push(&sha[..2]);
+        dirs.push(&sha[2..]);
+        let filename = repo.repo_files(dirs);
+
+        let file = File::open(filename).unwrap();
+        let file_reader = BufReader::new(file);
+        let mut zlib_reader = BufReader::new(ZlibDecoder::new(file_reader));
+
+        let mut object_type = Vec::new();
+        let mut object_size = Vec::new();
+        let mut object_content = Vec::new();
+
+        zlib_reader.read_until(0x20, &mut object_type).unwrap();
+        zlib_reader.read_until(0, &mut object_size).unwrap();
+        zlib_reader.read_to_end(&mut object_content).unwrap();
+
+        println!("Type: {}", String::from_utf8(object_type).unwrap());
+        println!("Size: {}", String::from_utf8(object_size).unwrap());
+        println!("Content: {}", String::from_utf8(object_content).unwrap());
     }
 
     fn repo_file<P: AsRef<Path>>(&self, name: P) -> PathBuf {
         self.git_dir.join(name)
+    }
+
+    fn repo_files<P, PI>(&self, name: PI) -> PathBuf
+        where PI: IntoIterator<Item=P>,
+              P: AsRef<Path>
+    {
+        let mut file = self.git_dir.clone();
+        name.into_iter().for_each(|p| file.push(p));
+        file
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use std::iter;
+    use std::path::PathBuf;
+
+    use crate::repo::GitRepository;
+
+    #[test]
+    fn repo_files() {
+        let files = &["objects/", "3b", "18e512dba79e4c8300dd08aeb37f8e728b8dad"];
+        let repo = GitRepository {
+            worktree: PathBuf::from("."),
+            git_dir: PathBuf::from(".git"),
+        };
+
+        assert_eq!(repo.repo_files(files),
+                   PathBuf::from(".git/objects/3b/18e512dba79e4c8300dd08aeb37f8e728b8dad"))
     }
 }
 
